a9cc442041f03f63d9f8097647c3ca1ac0d5b6fd
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-09-27 16:51:45 +0700

    update seeders

--- database/seeders/user_role_seeder.ts
+++ database/seeders/user_role_seeder.ts
@@ -13,8 +13,13 @@ export default class extends BaseSeeder {
       },
       {
         id: 2,
-        userRoleName: 'User',
-        userRoleDescription: 'User features',
+        userRoleName: '2FA App',
+        userRoleDescription: '2FA App features',
+      },
+      {
+        id: 3,
+        userRoleName: 'Authenticator',
+        userRoleDescription: 'Authenticator features',
       },
     ])
   }
